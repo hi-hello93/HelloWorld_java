dddd8aacb008e4a6c1081c5b6614b041cff2d749
방가은 <gaeun5990@naver.com>
2017-01-13 12:43:01 +0900

    업데이트1

--- HelloWorld/src/com/test/helloworld/HelloWorld.java
+++ HelloWorld/src/com/test/helloworld/HelloWorld.java
@@ -2,7 +2,8 @@ package com.test.helloworld;
 
 public class HelloWorld {
 	public static void main(String arg[]){
-		System.out.println("Hello, World~!");  j j nb
+		System.out.println("Hello, World~!");
+		System.out.println("안녕하세요.");
 	}
 
 }
